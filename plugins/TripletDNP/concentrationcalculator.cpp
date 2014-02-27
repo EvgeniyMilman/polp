757c06ac5b0fc21d71407f3a2d0cfe1e2e08021a
Evgeniy Milman <milman.evgeniy@gmail.com>
2014-02-27 20:23:01 +0900

    Implement ConcentrationCalculator

--- plugins/TripletDNP/concentrationcalculator.cpp
+++ plugins/TripletDNP/concentrationcalculator.cpp
@@ -2,8 +2,10 @@
 
 #include "ui_ConcentrationCalculator.h"
 
-ConcentrationCalculator::ConcentrationCalculator(QWidget *parent) :QWidget(parent){
+ConcentrationCalculator::ConcentrationCalculator(QWidget *parent) :QWidget(parent),ui(new Ui::ConcentrationCalculatorForm()){
     ui->setupUi(this);
+    ui->comboBox->setCurrentIndex(1);
+    ui->comboBox->setCurrentIndex(0);
 }
 
 QString ConcentrationCalculator::title(){
@@ -12,4 +14,70 @@ QString ConcentrationCalculator::title(){
 
 QWidget *ConcentrationCalculator::toolPane(){
     return this;
+}
+
+void ConcentrationCalculator::on_comboBox_currentIndexChanged(int index){
+   double molarMass[3] ={p_TERPHENYL_MOLAR_MASS,NAPHTHALENE_MOLAR_MASS, customMolarMass};
+    ui->molarmassSpinBox->setEnabled((index == 2));
+    ui->molarmassSpinBox->setValue(molarMass[index]);
+    ui->massPentaceneSpinBox->setValue(
+                calculatePentaceneMass(ui->molarmassSpinBox->value(),
+                                       ui->massDoubleSpinBox->value(),
+                                       ui->concentrationSpinBox->value()
+                ));
+
+}
+
+void ConcentrationCalculator::on_molarmassSpinBox_valueChanged(double arg1){
+    if(ui->molarmassSpinBox->isEnabled()){
+        customMolarMass = arg1;
+        ui->massPentaceneSpinBox->setValue(
+                    calculatePentaceneMass(ui->molarmassSpinBox->value(),
+                                           ui->massDoubleSpinBox->value(),
+                                           ui->concentrationSpinBox->value()
+                    ));
+    }
+}
+
+
+void ConcentrationCalculator::on_massDoubleSpinBox_valueChanged(double arg1){
+    ui->massPentaceneSpinBox->setValue(
+                calculatePentaceneMass(ui->molarmassSpinBox->value(),
+                                       ui->massDoubleSpinBox->value(),
+                                       ui->concentrationSpinBox->value()
+                ));
+}
+
+void ConcentrationCalculator::on_concentrationSpinBox_valueChanged(double arg1){
+    ui->massPentaceneSpinBox->setValue(
+                calculatePentaceneMass(ui->molarmassSpinBox->value(),
+                                       ui->massDoubleSpinBox->value(),
+                                       ui->concentrationSpinBox->value()
+                ));
+}
+
+void ConcentrationCalculator::on_massPentaceneSpinBox_valueChanged(double arg1){
+    ui->massDoubleSpinBox->setValue(
+                calculateHostMass(ui->molarmassSpinBox->value(),
+                                       ui->massPentaceneSpinBox->value(),
+                                       ui->concentrationSpinBox->value()
+                ));
+}
+
+double ConcentrationCalculator::calculatePentaceneMass(double hostMolarMass, double hostMass, double concentration){
+    double result;
+    double hostSubstanceAmount = hostMass / hostMolarMass;
+    concentration = concentration /100;
+    double pentaceneSubstanceAmount = concentration * hostSubstanceAmount / (1-concentration);
+    result = pentaceneSubstanceAmount * PENTACENE_MOLAR_MASS*1000;
+    return result;
+}
+
+double ConcentrationCalculator::calculateHostMass(double hostMolarMass, double pentaceneMass, double concentration){
+    double result;
+    concentration = concentration /100;
+    double pentaceneSubstanceAmount = pentaceneMass /PENTACENE_MOLAR_MASS /1000;
+    double hostSubstanceAmount = pentaceneSubstanceAmount *(1 - concentration)/concentration;
+    result = hostSubstanceAmount * hostMolarMass;
+    return result;
 }
--- plugins/TripletDNP/concentrationcalculator.h
+++ plugins/TripletDNP/concentrationcalculator.h
@@ -8,6 +8,13 @@ namespace Ui {
 class ConcentrationCalculatorForm;
 }
 
+#define PENTACENE_MOLAR_MASS 278.36 // g/mol
+
+#define p_TERPHENYL_MOLAR_MASS 230.30 // g/mol
+
+#define NAPHTHALENE_MOLAR_MASS 128.17 // g/mol
+
+
 class ConcentrationCalculator : public QWidget, public Tool
 {
     Q_OBJECT
@@ -20,8 +27,23 @@ signals:
 
 public slots:
 
+private slots:
+    void on_comboBox_currentIndexChanged(int index);
+
+    void on_molarmassSpinBox_valueChanged(double arg1);
+
+    void on_massDoubleSpinBox_valueChanged(double arg1);
+
+    void on_concentrationSpinBox_valueChanged(double arg1);
+
+    void on_massPentaceneSpinBox_valueChanged(double arg1);
+
 private:
     Ui::ConcentrationCalculatorForm * ui;
+    double customMolarMass;
+
+    double calculatePentaceneMass(double hostMolarMass, double hostMass, double concentration);
+    double calculateHostMass(double hostMolarMass, double pentaceneMass, double concentration);
 };
 
 #endif // CONCENTRATIONCALCULATOR_H
